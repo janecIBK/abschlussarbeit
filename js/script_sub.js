90357fb1fdf50c0eb37405772e75749650b3feba
janecIBK <csas2872@uibk.ac.at>
2016-06-13 23:06:19 +0200

    some how there´s just on layer on the map

--- js/script_sub.js
+++ js/script_sub.js
@@ -26,13 +26,14 @@ window.onload = function() {
 
 
 
-	var adlerKarte = L.map("adlerkarteDiv");
-	var hash = new L.Hash(adlerKarte);
+	var transalpKarte = L.map("transalpDiv");
+	var hash = new L.Hash(transalpKarte);
 	var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
-	}).addTo(adlerKarte);
-	adlerKarte.setView([47, 11], 10);
-	var el = L.control.elevation({collapsed: true}).addTo(adlerKarte);
+	}).addTo(transalpKarte);
+	 transalpKarte.setView([47, 11], 10);
+	var el = L.control.elevation({collapsed: true}).addTo(transalpKarte);
+	
 	var etappe01 = L.geoJson(etappe01json, {
 		style: {
 			color: "#ff0000",
@@ -40,15 +41,16 @@ window.onload = function() {
 		},
 		onEachFeature: el.addData.bind(el)
 	});
-	adlerKarte.addLayer(etappe01);
+	transalpKarte.addLayer(etappe01);
 
 	var etappe02 = L.geoJson(etappe02json, {
 		style: {
 			color: "##ff0000",
 			weight: 10
-		}
+		},
+		onEachFeature: el.addData.bind(el)
 	});
-	adlerKarte.addLayer(etappe02);
+	transalpKarte.addLayer(etappe02);
 
 	var etappe03 = L.geoJson(etappe03json, {
 		style: {
@@ -56,7 +58,7 @@ window.onload = function() {
 			weight: 10
 		}
 	});
-	adlerKarte.addLayer(etappe03);
+	transalpKarte.addLayer(etappe03);
 
 	var etappe04 = L.geoJson(etappe04json, {
 		style: {
@@ -64,25 +66,60 @@ window.onload = function() {
 			weight: 10
 		}
 	});
-	adlerKarte.addLayer(etappe04);
+	transalpKarte.addLayer(etappe04);
 
-	var etappenGruppe = L.featureGroup([etappe01, etappe02, etappe03, etappe04]);
-	adlerKarte.addLayer(etappenGruppe);
-	adlerKarte.fitBounds(etappenGruppe.getBounds());
+	var etappe05 = L.geoJson(etappe05json, {
+		style: {
+			color: "##ff0000",
+			weight: 10
+		}
+	});
+	transalpKarte.addLayer(etappe06);
+
+	var etappe06 = L.geoJson(etappe06json, {
+		style: {
+			color: "##ff0000",
+			weight: 10
+		}
+	});
+	transalpKarte.addLayer(etappe05);
+
+	var etappe06 = L.geoJson(etappe06json, {
+		style: {
+			color: "##ff0000",
+			weight: 10
+		}
+	});
+	transalpKarte.addLayer(etappe06);
+
+	var etappe07 = L.geoJson(etappe07json, {
+		style: {
+			color: "##ff0000",
+			weight: 10
+		}
+	});
+	transalpKarte.addLayer(etappe07);
+
+	var etappenGruppe = L.featureGroup([etappe01, etappe02, etappe03, etappe04, etappe05, etappe06, etappe07]);
+	transalpKarte.addLayer(etappenGruppe);
+	transalpKarte.fitBounds(etappenGruppe.getBounds());
 
 	L.control.layers({
 		"OSM": osmLayer,
 		"geoland": layers.bmapoverlay
 	}, {
 		"Tracks": etappenGruppe
-	}).addTo(adlerKarte);
+	}).addTo(transalpKarte);
 
-	adlerKarte.fitBounds(etappe01.getBounds());
+	transalpKarte.fitBounds(etappe01.getBounds());
 
 	etappe01.bindPopup("<b>Etappe 01</b>");
 	etappe02.bindPopup("<b>Etappe 02</b>");
 	etappe03.bindPopup("<b>Etappe 03</b>");
 	etappe04.bindPopup("<b>Etappe 04</b>");
+	etappe05.bindPopup("<b>Etappe 05</b>");
+	etappe06.bindPopup("<b>Etappe 06</b>");
+	etappe07.bindPopup("<b>Etappe 07</b>");
 
 
 	var bounds = etappenGruppe.getBounds();
@@ -93,18 +130,4 @@ window.onload = function() {
 		'&maxy=' + bounds.getNorth() +
 		'&size=mini_square&mapfilter=true&callback=zeigBilder';
 
-	var script = document.createElement("script");
-	script.src = url;
-	document.getElementsByTagName('head')[0].appendChild(script);
-	window.zeigBilder = function(data) {
-		for (var i = 0; i < data.photos.length; i++) {
-			console.log("panoramio fertig geladen: ", i, data.photos[i].photo_title);
-			L.marker([data.photos[i].latitude, data.photos[i].longitude], {
-					icon: L.icon({
-						iconUrl: data.photos[i].photo_file_url
-					})
-				}).bindPopup("<h2>" + data.photos[i].photo_title + "</h2><a href='" + data.photos[i].photo_url + "'>Link zum Bild</a>")
-				.addTo(adlerKarte);
-		}
-	}
 };
